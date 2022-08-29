3ddb13835b8bfc02ac1ce95424493586fd5e7d30
Justin <justin.tseng@kingwaytek.com>
2022-08-29 17:13:20 +0800

    complete distinct sections view controller

--- iOSTrainning/CollectionView/DistinctSectionsViewController.swift
+++ iOSTrainning/CollectionView/DistinctSectionsViewController.swift
@@ -11,6 +11,18 @@ class DistinctSectionsViewController: UIViewController {
 
     enum SectionLayoutKind: Int, CaseIterable {
         case list, grid5, grid3
+        var columnCount: Int {
+            switch self {
+            case .grid3:
+                return 3
+
+            case .grid5:
+                return 5
+
+            case .list:
+                return 1
+            }
+        }
     }
 
     var dataSource: UICollectionViewDiffableDataSource<SectionLayoutKind, Int>! = nil
@@ -27,9 +39,30 @@ class DistinctSectionsViewController: UIViewController {
 extension DistinctSectionsViewController {
     /// - Tag: PerSection
     func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
+                                                            layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
 
-        let layout = UICollectionViewLayout()
+            guard let sectionLayoutKind = SectionLayoutKind(rawValue: sectionIndex) else { return nil }
+            let columns = sectionLayoutKind.columnCount
 
+            // The group auto-calculates the actual item width to make
+            // the requested number of columns fit, so this widthDimension is ignored.
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+            let groupHeight = columns == 1 ?
+            NSCollectionLayoutDimension.absolute(44) :
+            NSCollectionLayoutDimension.fractionalWidth(0.2)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: groupHeight)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
+
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            return section
+        }
         return layout
     }
 }
@@ -44,6 +77,40 @@ extension DistinctSectionsViewController {
     }
     func configureDataSource() {
 
+        let listCellRegistration = UICollectionView.CellRegistration<ListCell, Int> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = "\(identifier)"
+        }
+
+        let textCellRegistration = UICollectionView.CellRegistration<TextCell, Int> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = "\(identifier)"
+            cell.contentView.backgroundColor = .link
+            cell.contentView.layer.borderColor = UIColor.black.cgColor
+            cell.contentView.layer.borderWidth = 1
+            cell.contentView.layer.cornerRadius = SectionLayoutKind(rawValue: indexPath.section)! == .grid5 ? 8 : 0
+            cell.label.textAlignment = .center
+            cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+        }
+
+        dataSource = UICollectionViewDiffableDataSource<SectionLayoutKind, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            // Return the cell.
+            return SectionLayoutKind(rawValue: indexPath.section)! == .list ?
+            collectionView.dequeueConfiguredReusableCell(using: listCellRegistration, for: indexPath, item: identifier) :
+            collectionView.dequeueConfiguredReusableCell(using: textCellRegistration, for: indexPath, item: identifier)
+        }
+
+        // initial data
+        let itemsPerSection = 10
+        var snapshot = NSDiffableDataSourceSnapshot<SectionLayoutKind, Int>()
+        SectionLayoutKind.allCases.forEach {
+            snapshot.appendSections([$0])
+            let itemOffset = $0.rawValue * itemsPerSection
+            let itemUpperbound = itemOffset + itemsPerSection
+            snapshot.appendItems(Array(itemOffset..<itemUpperbound))
+        }
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
 }
 
