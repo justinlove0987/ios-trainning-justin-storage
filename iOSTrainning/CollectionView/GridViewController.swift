d2db430d0ff742ec54e65be6557433e6c17fd9a1
Justin <justin.tseng@kingwaytek.com>
2022-08-29 15:21:24 +0800

    setup grid view controller

--- iOSTrainning/CollectionView/GridViewController.swift
+++ iOSTrainning/CollectionView/GridViewController.swift
@@ -9,12 +9,54 @@ import UIKit
 
 class GridViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-
-        
-        
+    enum Section {
+        case main
     }
 
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    var collectionView: UICollectionView! = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Grid"
+        configureHierarchy()
+        configureDataSource()
+    }
+}
+
+extension GridViewController {
+    /// - Tag: Grid
+}
+
+extension GridViewController {
+    private func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .black
+        view.addSubview(collectionView)
+    }
+    private func configureDataSource() {
+
+        let cellRegistration = UICollectionView.CellRegistration<TextCell, Int> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = "\(identifier)"
+            cell.contentView.backgroundColor = .link
+            cell.layer.borderColor = UIColor.black.cgColor
+            cell.layer.borderWidth = 1
+            cell.label.textAlignment = .center
+            cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+        }
+
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            // Return the cell.
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+        }
+
+        // initial data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(Array(0..<94))
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
 }
