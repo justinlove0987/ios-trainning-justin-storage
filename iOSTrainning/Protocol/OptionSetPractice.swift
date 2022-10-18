c86333e73d11c782855b3f2142ad89ff63b39284
Justin <justin.tseng@kingwaytek.com>
2022-10-18 17:12:44 +0800

    start OptionSet protocol practice

--- iOSTrainning/Protocol/OptionSetPractice.swift
+++ iOSTrainning/Protocol/OptionSetPractice.swift
@@ -7,22 +7,11 @@
 
 import UIKit
 
-struct Transportation: OptionSet {
-    static let car = Transportation(rawValue: 1<<0)
-    static let airplane = Transportation(rawValue: 1<<1)
-    static let bike = Transportation(rawValue: 1<<2)
-
-    let rawValue: Int
-}
-
 class OptionSetPractice: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let transportation: Transportation = [.airplane, .bike]
-
-        print(transportation.rawValue)
+        // 請實作OptionSet protocol的例子（程式碼）。
 
     }
 }
